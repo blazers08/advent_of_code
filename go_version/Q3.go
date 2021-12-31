0495ad24d0b356f7bcbc012093ca702f952735af
Denny <blazers20736@gmail.com>
2021-12-31 21:13:04 +0800

    Add q3 solution

---
+++ go_version/Q3.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+type Pair struct {
+	move  string
+	steps int
+}
+
+func read_input(fname string) ([]Pair, error) {
+	datas, err := ioutil.ReadFile(fname)
+	if err != nil {
+		return nil, err
+	}
+
+	data := strings.Split(string(datas), "\r\n")
+	cleaned_data := []Pair{}
+
+	for _, val := range data {
+		sorted := strings.Split(string(val), " ")
+		n, _ := strconv.Atoi(sorted[1])
+		cleaned_data = append(cleaned_data, Pair{sorted[0], n})
+	}
+	return cleaned_data, nil
+}
+
+func calculate(data []Pair) int {
+	var horizontal int = 0
+	var depth int = 0
+
+	for _, ele := range data {
+		if ele.move == "forward" {
+			horizontal += ele.steps
+		} else if ele.move == "down" {
+			depth += ele.steps
+		} else {
+			depth -= ele.steps
+		}
+	}
+
+	return horizontal * depth
+}
+
+func main() {
+	data, err := read_input("../input/q3input.in")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Println(data)
+	fmt.Println(calculate(data))
+}
