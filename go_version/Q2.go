171ecf138425824ca470e6239cffc85e52c6204c
Denny <blazers20736@gmail.com>
2021-12-31 19:24:35 +0800

    Add q2 solution

---
+++ go_version/Q2.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func read_input(fname string) ([]int, error) {
+	datas, err := ioutil.ReadFile(fname)
+	if err != nil {
+		return nil, err
+	}
+
+	data := strings.Split(string(datas), "\r\n")
+	numbers := make([]int, 0, len(data))
+
+	for _, ele := range data {
+		n, err := strconv.Atoi(ele)
+		if err != nil {
+			return nil, err
+		}
+		numbers = append(numbers, n)
+	}
+	return numbers, nil
+}
+
+func calcu_in_tro(data []int) int {
+	var current int
+	var increased int = 0
+
+	for i := 0; i < len(data)-3+1; i++ {
+		tmp := data[i] + data[i+1] + data[i+2]
+
+		if current == 0 {
+			current = tmp
+		} else {
+			if current < tmp {
+				increased += 1
+			}
+		}
+		current = tmp
+	}
+	return increased
+}
+
+func main() {
+	data, err := read_input("../input/q1input.in")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(calcu_in_tro(data))
+}
